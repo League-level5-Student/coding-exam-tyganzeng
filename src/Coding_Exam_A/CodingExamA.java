8be8aeafe36f6c87342a7b4340db41f908d9d6a4
league <league@imac-7.attlocal.net>
2019-02-06 18:28:14 -0800

    Finished problem A

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -1,6 +1,7 @@
 package Coding_Exam_A;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -22,5 +23,47 @@ public class CodingExamA {
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
 
+		//int num = 0;
+		String colorString = "red";
+		Color color = Color.RED;
+	//	int sides = 3;
+		colorString = JOptionPane.showInputDialog("What color? (red, green, blue)");
+		switch(colorString) {
+		case "red" : {
+			color = Color.RED; 
+			break;
+		}
+		case "green" : {
+			color = Color.GREEN; 
+			break;
+		}
+		default : {
+			color = Color.BLUE; 
+			break;
+		}
+		
+		}
+		final int num = Integer.parseInt(JOptionPane.showInputDialog("How many robots"));
+		final int sides = Integer.parseInt(JOptionPane.showInputDialog("How many sides"));
+		
+		ArrayList <Thread> threads = new ArrayList<Thread>();
+		for(int i = 0; i < num; i++) {
+			Robot r = new Robot(100 + 100 * i , 100);
+			r.setPenColor(color);
+			r.penDown();
+			Thread t1 = new Thread(()-> {
+				for(int j = 0; j < sides; j++) {
+					r.move(40);
+					int h = 180 * (sides - 2) / sides;
+					System.out.println(h);
+					r.turn(180 - h);
+				}
+				r.hide();
+			});
+			threads.add(t1);
+		}
+		for(Thread t : threads) {
+			t.start();
+		}
 	}
 }
